eb9d972d007446f9e3026a1fc95d6a014e97b661
Dylan Schoenmakers <d.schoenmakers@opifer.nl>
2016-03-30 14:18:15 +0200

    Added node packages

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,4 +20,22 @@ class AppKernel extends Kernel
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    public function getCacheDir()
+    {
+        if (strpos(__DIR__, 'vagrant') !== false && in_array($this->environment, array('dev'))) {
+            return '/dev/shm/symfony/cache/' .  $this->environment;
+        }
+
+        return parent::getCacheDir();
+    }
+
+    public function getLogDir()
+    {
+        if (strpos(__DIR__, 'vagrant') !== false && in_array($this->environment, array('dev'))) {
+            return '/dev/shm/symfony/logs';
+        }
+
+        return parent::getLogDir();
+    }
 }
